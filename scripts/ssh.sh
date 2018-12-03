64153d76f8eaef41c220f8fb059b8838084bc7aa
Zach Heineman <zach@heineman.info>
2018-12-02 20:43:28 -0800

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,1 +1,16 @@
 ssh-keygen -t rsa -b 4096 -C "zach@heineman.info"
+
+eval "$(ssh-agent -s)"
+
+vi ~/.ssh/config
+
+# Host *
+#  AddKeysToAgent yes
+#  UseKeychain yes
+#  IdentityFile ~/.ssh/id_rsa
+
+ssh-add -K ~/.ssh/id_rsa
+
+pbcopy < ~/.ssh/id_rsa.pub
+
+# Copy to github settings
