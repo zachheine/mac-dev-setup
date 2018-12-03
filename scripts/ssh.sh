163ce5af79aff331c4e699710b982b4c1c4e1709
Zach Heineman <zach@heineman.info>
2018-12-02 20:44:15 -0800

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,3 +1,5 @@
+# https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+
 ssh-keygen -t rsa -b 4096 -C "zach@heineman.info"
 
 eval "$(ssh-agent -s)"
@@ -10,6 +12,8 @@ vi ~/.ssh/config
 #  IdentityFile ~/.ssh/id_rsa
 
 ssh-add -K ~/.ssh/id_rsa
+
+# https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/
 
 pbcopy < ~/.ssh/id_rsa.pub
 
