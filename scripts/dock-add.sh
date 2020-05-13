dbd7c332e5313a851065a0aed0e51cc495a97078
Zach Heineman <zach@heineman.info>
2020-05-13 08:49:17 -0700

    Add the &&s

--- scripts/dock-add.sh
+++ scripts/dock-add.sh
@@ -1,22 +1,22 @@
-dockutil --remove 'Safari'
-dockutil --remove 'Mail'
-dockutil --remove 'FaceTime'
-dockutil --remove 'Messages'
-dockutil --remove 'Maps'
-dockutil --remove 'Photos'
-dockutil --remove 'Contacts'
-dockutil --remove 'Calendar'
-dockutil --remove 'Reminders'
-dockutil --remove 'Notes'
-dockutil --remove 'Music'
-dockutil --remove 'Podcasts'
-dockutil --remove 'TV'
-dockutil --remove 'News'
+dockutil --remove 'Safari' &&
+dockutil --remove 'Mail' &&
+dockutil --remove 'FaceTime' &&
+dockutil --remove 'Messages' &&
+dockutil --remove 'Maps' &&
+dockutil --remove 'Photos' &&
+dockutil --remove 'Contacts' &&
+dockutil --remove 'Calendar' &&
+dockutil --remove 'Reminders' &&
+dockutil --remove 'Notes' &&
+dockutil --remove 'Music' &&
+dockutil --remove 'Podcasts' &&
+dockutil --remove 'TV' &&
+dockutil --remove 'News' &&
 dockutil --remove 'App Store'
 
-dockutil --add /Applications/Google\ Chrome.app
-dockutil --add /Applications/Visual\ Studio\ Code.app
-dockutil --add /Applications/Adobe\ Photoshop\ 2020/Adobe\ Photoshop\ 2020.app
-dockutil --add /Applications/Adobe\ Illustrator\ 2020/Adobe\ Illustrator.app
-dockutil --add /System/Applications/Utilities/Terminal.app
+dockutil --add /Applications/Google\ Chrome.app &&
+dockutil --add /Applications/Visual\ Studio\ Code.app &&
+dockutil --add /Applications/Adobe\ Photoshop\ 2020/Adobe\ Photoshop\ 2020.app &&
+dockutil --add /Applications/Adobe\ Illustrator\ 2020/Adobe\ Illustrator.app &&
+dockutil --add /System/Applications/Utilities/Terminal.app &&
 dockutil --add /Applications/Xcode.app
