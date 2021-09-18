7fe234079d51809a7acfa41114f69b114a022d6a
Zach Heineman <zach@heineman.info>
2021-09-18 10:10:53 -0700

    Update the dock scripts

---
+++ scripts/dock-remove.sh
@@ -0,0 +1,18 @@
+dockutil --remove 'Safari' &&
+dockutil --remove 'Mail' &&
+dockutil --remove 'FaceTime' &&
+dockutil --remove 'Messages' &&
+dockutil --remove 'Maps' &&
+dockutil --remove 'Photos' &&
+dockutil --remove 'Contacts' &&
+dockutil --remove 'Calendar' &&
+dockutil --remove 'Reminders' &&
+dockutil --remove 'Notes' &&
+dockutil --remove 'Music' &&
+dockutil --remove 'Podcasts' &&
+dockutil --remove 'TV' &&
+dockutil --remove 'News' &&
+dockutil --remove 'App Store'
+dockutil --remove 'Keynote'
+dockutil --remove 'Numbers'
+dockutil --remove 'Pages'
